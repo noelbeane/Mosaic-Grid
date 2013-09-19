565174d966d5834d3cd8900f5ac312efe1b5127f
Noel Beane <noel@beane.biz>
2013-09-19 17:26:00 -0400

    grunt modules updated

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,35 +1,27 @@
 module.exports = function(grunt) {
-  
-  // Project configuration.
+
+ 
+////////////////////////////////////
+///////  PROJECT CONFIG
+///////////////////////////////////
+
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     
-    concat: {
-      options: {
-        separator: ';'
-      },
-      dist: {
-        src: ['src/**/*.js'],
-        dest: 'dist/<%= pkg.name %>.js'
-      }
-    },
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
-      },
-      dist: {
-        files: {
-          'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+    //qunit: {
+      //all: ['**/*.html']
+    //},
+    connect: {
+      server: {
+        options: {
+          //port: 9001,
+          //base: 'www-root'
         }
       }
     },
-    qunit: {
-      files: ['test/**/*.html']
-    },
     jshint: {
-      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+      all: ['js/**/*.js'],
       options: {
-        // options here to override JSHint defaults
         globals: {
           jQuery: true,
           console: true,
@@ -38,20 +30,67 @@ module.exports = function(grunt) {
         }
       }
     },
+    concat: {
+      options: {
+        stripBanners: true,
+        banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+        '<%= grunt.template.today("yyyy-mm-dd") %> */'
+      },
+      dist: {
+        src: ['js/src/**/*.js'],
+        dest: 'js/mosaicgrid.js'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      dist: {
+        files: {
+          'js/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+        }
+      }
+    },
+    sass: {
+      dist: {
+        files: {
+          'css/mosaicgrid.css': 'sass/mosaicgrid.scss',
+          'css/media_queries.css': 'sass/media_queries.scss'
+        }
+      }
+    },
     watch: {
-      files: ['<%= jshint.files %>'],
-      tasks: ['jshint', 'qunit']
+      scripts: {
+        files: 'js/**/*.js',
+        tasks: ['jshint'],
+        options: {
+          spawn: false,
+        }
+      },
+      css: {
+	      files: ['sass/*.scss'],
+	      tasks: ['sass'],
+      }
     }
   });
 
+
+////////////////////////////////////
+///////  PLUGINS
+///////////////////////////////////
+
+  grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-qunit');
+  //grunt.loadNpmTasks('grunt-contrib-qunit');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-concat');
-
-  grunt.registerTask('test', ['jshint', 'qunit']);
-
-  grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  
+  
+////////////////////////////////////
+///////  TASKS
+///////////////////////////////////
+  grunt.registerTask('default', ['connect', 'jshint', 'concat', 'watch', /*'qunit',*/ 'uglify', 'sass']);
 
 };
---
+++ js/mosaicgrid.js
@@ -0,0 +1,5 @@
+/*! mosaicgrid - v0.0.0 - 2013-09-19 */var test01= "mosaicgrid";
+
+
+
+var test02= "tiles";
