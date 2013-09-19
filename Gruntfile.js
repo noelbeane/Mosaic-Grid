0b7a8c3a69338e3bc67588397d86be6ab878a099
Noel Beane <noel@beane.biz>
2013-09-18 21:49:15 -0400

    added grunt config settings

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,9 @@
 module.exports = function(grunt) {
-
+  
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    
     uglify: {
       options: {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
@@ -11,13 +12,35 @@ module.exports = function(grunt) {
         src: 'src/<%= pkg.name %>.js',
         dest: 'build/<%= pkg.name %>.min.js'
       }
+    },
+    
+    compass: {                  // Task
+      dist: {                   // Target
+        options: {              // Target options
+          sassDir: 'sass',
+          cssDir: 'css',
+          environment: 'production'
+        }
+      },
+      dev: {                    // Another target
+        options: {
+          sassDir: 'sass',
+          cssDir: 'css'
+        }
+      }
+    },
+    
+    jshint: {
+      myFiles: ['js/**/*.js', 'bower_components/**/*.js', 'node_modules/**/*.js']
     }
-  });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-jshint');#sthash.P2mKurAb.dpuf
 
   // Default task(s).
   grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['jshint', 'compass']);
 
 };
