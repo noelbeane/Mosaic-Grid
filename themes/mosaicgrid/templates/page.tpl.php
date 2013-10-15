8d453f43ed6900127d7bf7bdf6430df119b6522b
Noel Beane <noel@beane.biz>
2013-10-15 17:00:23 -0400

    drupal templates updated

--- themes/mosaicgrid/templates/page.tpl.php
+++ themes/mosaicgrid/templates/page.tpl.php
@@ -87,161 +87,192 @@
 ?>
 <div id="page-wrapper"><div id="page">
 
-  <div id="header" class="<?php print $secondary_menu ? 'with-secondary-menu': 'without-secondary-menu'; ?>"><div class="section clearfix">
 
-    <?php if ($logo): ?>
-      <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
-        <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
-      </a>
-    <?php endif; ?>
+<!-- //////////////////////// START HEADER ///////////////////////// -->
+  <div id="header" class="<?php print $secondary_menu ? 'with-secondary-menu': 'without-secondary-menu'; ?>">
+      
+         <div class="row clearfix" id="row_header">
+			<div class="rail">&nbsp;</div>
+			<div class="header-wrapper">
+				<header>
+					<div class="sitelogo-wrapper" id="sitelogo">
+						<div class="sitelogo">
+							
+							<?php if ($logo): ?>
+							  <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
+								  <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+							  </a>
+							<?php endif; ?>
+							
+						</div>
+						<div class="menubtn-wrapper" id="sitemenuBtn" onclick="topnavSlide('menu');">
+						  <div class="icon-menu icomoon"></div>
+						</div>
+					</div>
+					<div class="searchbox-wrapper" id="searchbox">
+						<label class="input searchinput">
+							<span style="color: rgb(153, 153, 153);">Search</span>
+							<input type="text" />
+						</label>
+					</div>
+					<div class="toolbox-wrapper" id="toolbox">
+						<div class="toolboxBtn" id="toolboxBtn" onclick="topnavSlide('tools');">
+							<div class="icon-cog icomoon">
+						</div>
+					</div>
+				</header>
+			</div>
+			<div class="rail">&nbsp;</div>
+		</div>    
+    
+        <!-- ////////////////////////////////////  DROPDOWN ROW  ///////////////////////////////////// -->
+		<div class="row clearfix" id="row_drawer">
+			<div class="rail">&nbsp;</div>
+			<div class="header-wrapper">
+			<div class="drawer-wrapper tranz_norm" id="drawer">
+				
+				<div class="mainnavList" id="mainnav">					
+					<?php if ($main_menu): ?>
+					<div id="main-menu" class="navigation">
+						<?php print theme('links__system_main_menu', array(
+							'links' => $main_menu,
+							'attributes' => array(
+							'id' => 'main-menu-links',
+							'class' => array('links', 'clearfix'),
+							),
+							'heading' => array(
+							'text' => t('Main menu'),
+							'level' => 'h2',
+							'class' => array('element-invisible'),
+							),
+						)); ?>
+				    </div>
+					<?php endif; ?>				
+				</div>
+				
+				<div class="toolboxList" id="tools">					
+					<?php if ($secondary_menu): ?>
+					<div id="secondary-menu" class="navigation">
+						<?php print theme('links__system_secondary_menu', array(
+							'links' => $secondary_menu,
+							'attributes' => array(
+							'id' => 'secondary-menu-links',
+							'class' => array('links', 'inline', 'clearfix'),
+							),
+							'heading' => array(
+							'text' => t('Secondary menu'),
+							'level' => 'h2',
+							'class' => array('element-invisible'),
+							),
+						)); ?>
+					</div>
+					
+					<?php endif; ?>
+					
+					<?php if (!$secondary_menu): ?>
+					  <?php print render($form = drupal_get_form('user_login_block'));?>
+					<?php endif; ?>
+				</div>
+			</div>
+			</div>
+			<div class="rail">&nbsp;</div>
+		</div>
 
-    <?php if ($site_name || $site_slogan): ?>
-      <div id="name-and-slogan"<?php if ($hide_site_name && $hide_site_slogan) { print ' class="element-invisible"'; } ?>>
-
-        <?php if ($site_name): ?>
-          <?php if ($title): ?>
-            <div id="site-name"<?php if ($hide_site_name) { print ' class="element-invisible"'; } ?>>
-              <strong>
-                <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-              </strong>
-            </div>
-          <?php else: /* Use h1 when the content title is empty */ ?>
-            <h1 id="site-name"<?php if ($hide_site_name) { print ' class="element-invisible"'; } ?>>
-              <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-            </h1>
-          <?php endif; ?>
-        <?php endif; ?>
-
-        <?php if ($site_slogan): ?>
-          <div id="site-slogan"<?php if ($hide_site_slogan) { print ' class="element-invisible"'; } ?>>
-            <?php print $site_slogan; ?>
-          </div>
-        <?php endif; ?>
-
-      </div> <!-- /#name-and-slogan -->
-    <?php endif; ?>
 
     <?php print render($page['header']); ?>
+</div>
+<!-- //////////////////////// END HEADER ///////////////////////// -->
 
-    <?php if ($main_menu): ?>
-      <div id="main-menu" class="navigation">
-        <?php print theme('links__system_main_menu', array(
-          'links' => $main_menu,
-          'attributes' => array(
-            'id' => 'main-menu-links',
-            'class' => array('links', 'clearfix'),
-          ),
-          'heading' => array(
-            'text' => t('Main menu'),
-            'level' => 'h2',
-            'class' => array('element-invisible'),
-          ),
-        )); ?>
-      </div> <!-- /#main-menu -->
-    <?php endif; ?>
 
-    <?php if ($secondary_menu): ?>
-      <div id="secondary-menu" class="navigation">
-        <?php print theme('links__system_secondary_menu', array(
-          'links' => $secondary_menu,
-          'attributes' => array(
-            'id' => 'secondary-menu-links',
-            'class' => array('links', 'inline', 'clearfix'),
-          ),
-          'heading' => array(
-            'text' => t('Secondary menu'),
-            'level' => 'h2',
-            'class' => array('element-invisible'),
-          ),
-        )); ?>
-      </div> <!-- /#secondary-menu -->
-    <?php endif; ?>
 
-  </div></div> <!-- /.section, /#header -->
-
-  <?php if ($messages): ?>
-    <div id="messages"><div class="section clearfix">
-      <?php print $messages; ?>
-    </div></div> <!-- /.section, /#messages -->
-  <?php endif; ?>
-
-  <?php if ($page['featured']): ?>
-    <div id="featured"><div class="section clearfix">
-      <?php print render($page['featured']); ?>
-    </div></div> <!-- /.section, /#featured -->
-  <?php endif; ?>
-
+<!-- //////////////////////// START MAIN ///////////////////////// -->
   <div id="main-wrapper" class="clearfix"><div id="main" class="clearfix">
 
-    <?php if ($breadcrumb): ?>
-      <div id="breadcrumb"><?php print $breadcrumb; ?></div>
-    <?php endif; ?>
+            <div class="subnav_wrapper tranz_norm" id="subnav" onclick="subnavSlide();">
+				<div class="subnavBtn" id="subnavBtn"><div class="icon-tag icomoon"></div></div>
+				<div class="subnavList tranz_fast" id="subnavList">
+					<!--<ul>
+						<li>Tag Lorem Ipsum 01</li>
+						<li>Tag Lorem Ipsum 02</li>
+						<li>Tag Lorem Ipsum 03</li>
+					</ul>-->
+				</div>				
+			</div>
+			<div class="socnet_wrapper" id="socnet">
+				<ul>
+					<li><div class="icoframe"><div class="icon-facebook icomoon"></div></li>		
+					<li><div class="icoframe"><div class="icon-twitter icomoon"></div></li>
+					<li><div class="icoframe"><div class="icon-instagram icomoon"></div></li>
+					<li><div class="icoframe"><div class="icon-vimeo icomoon"></div></li>
+					<li><div class="icoframe"><div class="icon-github icomoon"></div></li>
+				</ul>
+			</div>
+			<div class="main_content_wrapper tranz_norm" id="mainContent">
+			  <div class="mosaic_container">
+			   <!-- //////////////////////// START MASONRY ///////////////////////// -->
+			   <!--<div class="masonry js-masonry mosaic_container" id='mosaic' data-masonry-options='{ "columnWidth": 10, "itemSelector": ".msnry_item" "gutter": 20, "isFitWidth": true, "isOriginLeft": true, "isOriginTop": true, "transitionDuration": "0.4s" }'></div>			  
+			   </div>-->
+			   <!-- //////////////////////// END MASONRY ///////////////////////// -->
+    
+			   <?php if ($messages): ?>
+			     <div id="messages"><div class="section clearfix">
+					 <?php print $messages; ?>
+					 </div></div> <!-- /.section, /#messages -->
+			   <?php endif; ?>
+    
+			   <?php if ($breadcrumb): ?>
+			     <div id="breadcrumb"><?php print $breadcrumb; ?></div>
+			   <?php endif; ?>
+			   
+			   <div id="content" class="column"><div class="section">
+      
+				   <a id="main-content"></a>
+				   <?php print render($title_prefix); ?>
+				   <?php if ($title): ?>
+				     <h1 class="title" id="page-title"><?php print $title; ?></h1>
+				   <?php endif; ?>
+				   <?php print render($title_suffix); ?>
+				   <?php if ($tabs): ?>
+				     <div class="tabs"><?php print render($tabs); ?></div>
+				   <?php endif; ?>
+				   <?php print render($page['help']); ?>
+				   <?php if ($action_links): ?>
+				     <ul class="action-links">
+					   <?php print render($action_links); ?>
+				     </ul>
+				   <?php endif; ?>
+				   <?php print render($page['content']); ?>
+				   <?php print $feed_icons; ?>
+      
+			   </div></div> 
+			   
+		      </div>			    	  
+	        </div>
 
-    <?php if ($page['sidebar_first']): ?>
-      <div id="sidebar-first" class="column sidebar"><div class="section">
-        <?php print render($page['sidebar_first']); ?>
-      </div></div> <!-- /.section, /#sidebar-first -->
-    <?php endif; ?>
-
-    <div id="content" class="column"><div class="section">
-      <?php if ($page['highlighted']): ?><div id="highlighted"><?php print render($page['highlighted']); ?></div><?php endif; ?>
-      <a id="main-content"></a>
-      <?php print render($title_prefix); ?>
-      <?php if ($title): ?>
-        <h1 class="title" id="page-title">
-          <?php print $title; ?>
-        </h1>
-      <?php endif; ?>
-      <?php print render($title_suffix); ?>
-      <?php if ($tabs): ?>
-        <div class="tabs">
-          <?php print render($tabs); ?>
-        </div>
-      <?php endif; ?>
-      <?php print render($page['help']); ?>
-      <?php if ($action_links): ?>
-        <ul class="action-links">
-          <?php print render($action_links); ?>
-        </ul>
-      <?php endif; ?>
-      <?php print render($page['content']); ?>
-      <?php print $feed_icons; ?>
-
-    </div></div> <!-- /.section, /#content -->
-
-    <?php if ($page['sidebar_second']): ?>
-      <div id="sidebar-second" class="column sidebar"><div class="section">
-        <?php print render($page['sidebar_second']); ?>
-      </div></div> <!-- /.section, /#sidebar-second -->
-    <?php endif; ?>
 
   </div></div> <!-- /#main, /#main-wrapper -->
+<!-- //////////////////////// END MAIN ///////////////////////// -->
+  
 
-  <?php if ($page['triptych_first'] || $page['triptych_middle'] || $page['triptych_last']): ?>
-    <div id="triptych-wrapper"><div id="triptych" class="clearfix">
-      <?php print render($page['triptych_first']); ?>
-      <?php print render($page['triptych_middle']); ?>
-      <?php print render($page['triptych_last']); ?>
-    </div></div> <!-- /#triptych, /#triptych-wrapper -->
-  <?php endif; ?>
 
+  
+<!-- //////////////////////// START FOOTER ///////////////////////// -->
   <div id="footer-wrapper"><div class="section">
 
-    <?php if ($page['footer_firstcolumn'] || $page['footer_secondcolumn'] || $page['footer_thirdcolumn'] || $page['footer_fourthcolumn']): ?>
-      <div id="footer-columns" class="clearfix">
-        <?php print render($page['footer_firstcolumn']); ?>
-        <?php print render($page['footer_secondcolumn']); ?>
-        <?php print render($page['footer_thirdcolumn']); ?>
-        <?php print render($page['footer_fourthcolumn']); ?>
-      </div> <!-- /#footer-columns -->
-    <?php endif; ?>
-
     <?php if ($page['footer']): ?>
-      <div id="footer" class="clearfix">
+      <footer id="footer">
+        <h2>Mosaic Grid  |  <a href="http://www.beane.biz">www.beane.biz</a></h2>
+        <div class="clearfix">
         <?php print render($page['footer']); ?>
-      </div> <!-- /#footer -->
+        </div>
+      </footer>
+      <!-- /#footer -->
     <?php endif; ?>
 
-  </div></div> <!-- /.section, /#footer-wrapper -->
+  </div></div>
+<!-- //////////////////////// END FOOTER ///////////////////////// -->
+
+
+
 
 </div></div> <!-- /#page, /#page-wrapper -->
